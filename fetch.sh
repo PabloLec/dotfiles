0f215a8f69ea4d0c5066019038f56af163693715
PabloLec <pablo.lecolinet@gmail.com>
2021-08-23 11:27:44 +0200

    Add tmux and modify fetch

--- fetch.sh
+++ fetch.sh
@@ -2,6 +2,15 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+available_files=( vscode terminator zsh p10k services qbt tmux)
+files_vscode=( "$HOME/.config/Code/User/settings.json" )
+files_terminator=( "$HOME/.config/terminator/config" )
+files_zsh=( "$HOME/.zshrc" )
+files_p10k=( "$HOME/.p10k.zsh" )
+files_services=( "/etc/systemd/system/vncserver@.service" )
+files_qbt=( "$HOME/.config/qBittorrent/qBittorrent.conf" )
+files_tmux=( "$HOME/.tmux.conf" )
+
 create_dir () {
   if mkdir "$SCRIPT_DIR/$1" 2> /dev/null ; then
       echo " - Created dir $1"
@@ -19,14 +28,28 @@ fetch () {
   for ((i = 2; i <= $#; i++ )); do
     copy $1 "${!i}"
   done
-  
 }
 
-fetch vscode "$HOME/.config/Code/User/settings.json"
-fetch terminator "$HOME/.config/terminator/config"
-fetch zsh "$HOME/.zshrc"
-fetch p10k "$HOME/.p10k.zsh"
-fetch services "/etc/systemd/system/vncserver@.service"
-fetch qbittorrent "$HOME/.config/qBittorrent/qBittorrent.conf"
+fetch_all () {
+	fetch vscode $files_vscode
+	fetch terminator $files_terminator
+	fetch zsh $files_zsh
+	fetch p10k $files_p10k
+	fetch services $files_services
+	fetch qbt $files_qbt
+}
 
+if [ $# == 1 ] && [ $1 == "all" ]; then
+   fetch_all
+   exit 0
+fi
 
+for ((j = 1; j <= $#; j++ )); do
+    arg="${!j}"
+    if (printf '%s\n' "${available_files[@]}" | grep -xq $arg); then
+        files=files_${arg}
+        fetch $arg ${!files}
+    else
+    	echo " ! Cannot find: $arg"
+    fi
+done
